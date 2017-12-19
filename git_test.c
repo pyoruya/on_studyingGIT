01fd13f608bdb145e7f6b434645ed1632c2dc190
pyoruya <pyoruya@gmail.com>
2017-12-19 16:27:01 +0900

    Just test for usage git
    git 연습.

--- git_test.c
+++ git_test.c
@@ -1,8 +1,21 @@
 #include "stdio.h"
+#include "math.h"
+
+double compensation_temperature(uint32_t ad, int temp, uint8_t code);
 
 int main(){
 	printf("Heelo World");
 	return 1;
+}
+
+double compensation_temperature(uint32_t ad, int temp, uint8_t code)
+{
+	double glc;
+	glc = ad * (1+(23-temp)*1.8/100);
+	glc -= intercept[code];
+	glc /= slope[code];
+	
+	return glc;
 }
 
 
